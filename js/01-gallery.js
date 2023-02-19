c03ebf06b37ca2d7ff523444933df4595a940bc6
Babytskyi-Kolya <112752847+Babytskyi-Kolya@users.noreply.github.com>
2023-02-19 13:54:25 +0300

    finish-work

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
+// import * as basicLightbox from 'basiclightbox';
 // Change code below this line
 
-console.log(galleryItems);
+const containerGallery = document.querySelector('.gallery');
+containerGallery.addEventListener('click', onClickShowOriginalImage)
+
+
+const listItemsMarkup = createItemGalere(galleryItems);
+containerGallery.innerHTML = listItemsMarkup;
+
+function createItemGalere (items){
+   return items.map(({preview, original, description}) =>{
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+   }).join('');
+}
+
+function onClickShowOriginalImage (evt){
+    evt.preventDefault();
+
+    if(!evt.target.classList.contains('gallery__image')){
+        return;
+    };
+    
+    const instance = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}" width="800" height="auto">`,
+    {
+    onShow: () => document.addEventListener("keydown", onCloseImage),
+    onClose: () => document.removeEventListener("keydown", onCloseImage),
+    }
+    );
+    instance.show()
+    function onCloseImage (event) {
+        if(event.code === 'Escape'){
+        instance.close();
+        }
+    };
+}
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const containerGallery = document.querySelector('.gallery');
+const listItemsMarkup = createItemGalere(galleryItems);
+
+containerGallery.insertAdjacentHTML("beforeend", listItemsMarkup);
+containerGallery.addEventListener('click', onClickShowOriginalImage)
+
+function createItemGalere (items){
+    return items.map(({preview, original, description}) =>{
+     return `<li class="gallery__item">
+     <a  href="${original}">
+     <img class="gallery__image" src="${preview}" 
+     alt="${description}" />
+   </a>
+   </li>`
+    }).join('');
+ }
+ 
+ function onClickShowOriginalImage (evt){
+    evt.preventDefault();
+
+    var lightbox = new SimpleLightbox('.gallery__item a', { 
+        captionsData: "alt",
+        captionDelay: 250,
+     });
+}
